83bd0af8453e33b50687cf1a9e4c9cad5c04544f
AATrubakova <Trubakovaaa@gmail.com>
2021-11-29 00:16:26 +0300

    homework 7.1

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,19 +1,15 @@
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.Scanner;
 
 public class Application {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
         System.out.println("Введите название города:");
 
         Scanner scanner = new Scanner(System.in);
         String city = scanner.nextLine();
         String cityCode = RequestHandler.detectCityId(city);
-        System.out.println("В городе " + city + ": ");
-        String foreast = RequestHandler.getForecast(cityCode);
-
-
-//        System.out.println(foreast);
-
+        String foreast = RequestHandler.getForecast(cityCode, city);
 
     }
 }
--- src/main/java/RequestHandler.java
+++ src/main/java/RequestHandler.java
@@ -7,6 +7,11 @@ import com.squareup.okhttp.Response;
 import org.w3c.dom.ls.LSOutput;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class RequestHandler {
     final static OkHttpClient okHttpClient = new OkHttpClient();
@@ -38,7 +43,7 @@ public class RequestHandler {
         return code;
     }
 
-    public static String getForecast(String cityCode) throws IOException {
+    public static String getForecast(String cityCode, String cityName) throws IOException, ParseException {
 
 
         HttpUrl httpUrl = new HttpUrl.Builder()
@@ -62,15 +67,19 @@ public class RequestHandler {
         String json = response.body().string();
 
         String forecastsJson = objectMapper.readTree(json).at("/DailyForecasts").toString();
-            for (int i = 0; i < 5; i++) {
-                String dayForecast = objectMapper.readTree(forecastsJson).get(i).toString();
-                String weatherAtDay = objectMapper.readTree(dayForecast).at("/Day").at("/IconPhrase").asText();
-                String weatherAtNight = objectMapper.readTree(dayForecast).at("/Night").at("/IconPhrase").asText();
-                String maxTemp = objectMapper.readTree(dayForecast).at("/Temperature").at("/Maximum").at("/Value").asText();
-                String minTemp = objectMapper.readTree(dayForecast).at("/Temperature").at("/Minimum").at("/Value").asText();
-                String date = objectMapper.readTree(dayForecast).at("/Date").asText();
-                System.out.println( " на  дату " + date + " ожидается днем " + weatherAtDay + ", ночью " +weatherAtNight + ", максимальная температура "+ maxTemp + ", минимальная температура " + minTemp);
-            }
+
+        DateFormat dfFrom = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
+        DateFormat dfTo = new SimpleDateFormat("dd.MM.yyyy");
+        for (int i = 0; i < 5; i++) {
+            String dayForecast = objectMapper.readTree(forecastsJson).get(i).toString();
+            String weatherAtDay = objectMapper.readTree(dayForecast).at("/Day").at("/IconPhrase").asText();
+            String weatherAtNight = objectMapper.readTree(dayForecast).at("/Night").at("/IconPhrase").asText();
+            String maxTemp = objectMapper.readTree(dayForecast).at("/Temperature").at("/Maximum").at("/Value").asText();
+            String minTemp = objectMapper.readTree(dayForecast).at("/Temperature").at("/Minimum").at("/Value").asText();
+            String strDate = objectMapper.readTree(dayForecast).at("/Date").asText();
+            Date date = dfFrom.parse(strDate);
+            System.out.println("В городе " + cityName + " на дату " + dfTo.format(date) + " ожидается днем " + weatherAtDay + ", ночью " + weatherAtNight + ", максимальная температура " + maxTemp + ", минимальная температура " + minTemp);
+        }
 
         return json;
     }
